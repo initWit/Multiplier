9e6e2eb54a8d14a21194f1f4488d1acf523dd6e5
initWit <figueras.robert@gmail.com>
2014-05-12 15:15:23 -0500

    Initial Commit

---
+++ Multiply/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Multiply
+//
+//  Created by Robert Figueras on 5/12/14.
+//  Copyright (c) 2014 AppSpaceship. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Multiply/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Multiply
+//
+//  Created by Robert Figueras on 5/12/14.
+//  Copyright (c) 2014 AppSpaceship. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
